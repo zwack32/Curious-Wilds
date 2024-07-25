91216527d7b21a4dfddb57a425c98234f5214b87
Zack Bayrer <105024677+zwack32@users.noreply.github.com>
2024-07-25 12:42:27 -0700

    added basic survival calculations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,23 @@
-extends Node
+extends CharacterBody2D
 
+#basic game loop variables
+var health = 100
+var player_temperature = 70
+var water = 100
+var energy = 2000
+
+#move to other script ASAP
+var time = 0 #midnight
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print ("test")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time += delta * 4
+	
+	energy -= 2000/1440 * delta * 4
+	
+	print (energy)
