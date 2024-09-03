5b3139c848a2835dc443e5dd8d1280821446c35c
Zack Bayrer <105024677+zwack32@users.noreply.github.com>
2024-09-03 10:24:11 -0500

    made ui better, tweaked numbers

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,18 +3,18 @@ extends CharacterBody2D
 #basic game loop variables
 var health = 100.0 #arbitrary
 var player_temperature = 70.0 #freedom units
-var water = 800.0 #centicups
-var energy = 2000.0 #Kcals
+var water = 1200.0 #centicups
+var energy = 30000.0 #Kcals
 
 var speed = 200.0
 
 var clothing_temperature = 0.0 #naked for now i guess
 var day_length = 1440 #in seconds #eventually get this from game_manager
 
-@onready var health_bar = $HealthBar
-@onready var temperature_bar = $TemperatureBar
-@onready var water_bar = $WaterBar
-@onready var energy_bar = $EnergyBar
+@onready var health_bar = $PlayerCaerma/HealthBar
+@onready var temperature_bar = $PlayerCaerma/TemperatureBar
+@onready var water_bar = $PlayerCaerma/WaterBar
+@onready var energy_bar = $PlayerCaerma/EnergyBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -61,8 +61,9 @@ func check_vitals(delta):
 	
 	if energy < 200:
 		health -= 10/day_length
-	if water < 200:
-		health -= 25/day_length
+	if water < 100:
+		pass
+		#health -= 20/day_length
 		
 func update_bars():
 	health_bar.value = health
